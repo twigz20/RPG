36ef9fc1e45bebdc9d4aefe7b165125d5d5b5852
Jerry Goguette <jerry_h16@hotmail.com>
2020-07-23 18:46:26 -0400

    Initial Commit after 1st Course

---
+++ RPG Project/Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,42 @@
+﻿using RPG.Saving;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+
+{
+    public class CinematicTrigger : MonoBehaviour, ISaveable
+    {
+        bool alreadyTriggered = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!alreadyTriggered && other.gameObject.tag == "Player")
+            {
+                GetComponent<PlayableDirector>().Play();
+                alreadyTriggered = true;
+            }
+        }
+
+        [System.Serializable]
+        struct CinematicsSaveData
+        {
+            public bool alreadyTriggered;
+        }
+
+        public object CaptureState()
+        {
+            CinematicsSaveData data = new CinematicsSaveData();
+            data.alreadyTriggered = alreadyTriggered;
+            return data;
+        }
+
+        public void RestoreState(object state)
+        {
+            CinematicsSaveData data = (CinematicsSaveData)state;
+            alreadyTriggered = data.alreadyTriggered;
+        }
+    }
+}
---
+++ RPG Project/Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using GameDevTV.Utils;
+using Hellmade.Sound;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] AudioClip backgroundAudioClip = null;
+        private Audio backgroundAudio = null;
+        private int backgroundAudioID = -1;
+
+        private void Awake()
+        {
+            if (backgroundAudioClip != null)
+            {
+                backgroundAudioID = EazySoundManager.PrepareMusic(backgroundAudioClip, 0.35f, true, false, 0.5f, 1);
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (backgroundAudioClip != null)
+            {
+                backgroundAudio = EazySoundManager.GetAudio(backgroundAudioID);
+                backgroundAudio.Play();
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
